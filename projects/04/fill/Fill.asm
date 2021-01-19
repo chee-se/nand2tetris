c7ba5fc3873b7a7906fd7120b557d8dcecde59f6
Yukihiro Chiba <chiba.yukihiro.biz@gmail.com>
2021-01-20 02:43:45 +0900

    feat: 04 finished

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,58 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// initialize SCREEN_END
+@SCREEN
+D=A
+@8192
+D=D+A
+@SCREEN_END
+M=D
+
+(LOOP)
+// initialize
+@SCREEN
+D=A
+@render
+M=D
+@color
+M=0
+
+@KBD
+D=M
+
+@DISPLAY
+D;JEQ // render white
+
+@color
+M=!M // render black
+
+// render from here
+(DISPLAY)
+
+// render color
+@color
+D=M
+@render
+A=M
+M=D
+
+// move to next word
+@render
+MD=M+1
+
+@SCREEN_END
+D=M-D
+
+@DISPLAY
+D;JGT
+
+@LOOP
+0;JMP
